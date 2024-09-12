cfe3d783f13ce93cd26cd05667abfe6e8935e065
Evgeniy Sidorenko <zarod2005@yandex.ru>
2024-09-12 12:45:18 +0300

    Re-phrased `-spewmissingassets` description to be more accurate

--- mp/src/utils/bspconverter/bspconverter.cpp
+++ mp/src/utils/bspconverter/bspconverter.cpp
@@ -100,7 +100,7 @@ void PrintUsage( int argc, char** argv )
 		"  -o (or -output)    : Specifies output file name, defaults to <mapname>_fixed.bsp\n"
 		"  -nolightdata       : Doesn't save any light information in the fixed file\n"
 		"  -keeplightmapalpha : Doesn't strip unused lightmap alpha data\n"
-		"  -spewmissingassets : Logs every missing brush texture and static prop model\n"
+		"  -spewmissingassets : Logs every missing brush material and static prop model\n"
 		"  -v (or -verbose)   : Turn on verbose output\n"
 		"\n"
 	);
